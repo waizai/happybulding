dd7ce0f1c33ed61c2e647d1198a311b343459ff5
dangxiaoli <dangxiaoli@meituan.com>
2018-01-03 10:45:28 +0800

    add

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/*
+* @Author: dangxiaoli
+* @Date:   2018-01-02 13:43:31
+* @Last Modified by:   dangxiaoli
+* @Last Modified time: 2018-01-02 13:43:38
+*/
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // 将你的默认的任务代码放在这
+});
