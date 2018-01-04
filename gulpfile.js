de1d4b5630d4f84b6dbc339c8ceb31203403b58c
dangxiaoli <dangxiaoli@meituan.com>
2018-01-04 22:19:57 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,41 @@
 * @Author: dangxiaoli
 * @Date:   2018-01-02 13:43:31
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2018-01-02 13:43:38
+* @Last Modified time: 2018-01-04 21:25:21
 */
 var gulp = require('gulp');
+const less = require('gulp-less');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS  = require('gulp-clean-css');
 
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
+
+gulp.task('clean', () => {
+    del.sync('build');
 });
+
+
+gulp.task('less', () => {
+    gulp.src('src/**/*.less')
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions', 'Firefox > 20'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('build'));
+});
+
+
+gulp.task('default', ['clean', 'less'], () => {
+    console.log('done!');
+});
+
+
+gulp.task('watch', () => {
+    const watcher = gulp.watch('src/**/*', ['default']);
+    watcher.on('change', event => {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
---
+++ src/script/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: dangxiaoli
+* @Date:   2018-01-04 20:45:28
+* @Last Modified by:   dangxiaoli
+* @Last Modified time: 2018-01-04 22:19:36
+*/
+const fn = (x, y) => {
+
+}
